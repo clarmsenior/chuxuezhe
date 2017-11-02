c6428d979fbf9aa0aff335dc327f27f719ef99fc
lavor <1371213115@qq.com>
2017-11-02 18:09:03 +0800

    cardview

--- qingju/app/src/main/java/com/example/mayn/qingju/assignment/AssignmentFragment.java
+++ qingju/app/src/main/java/com/example/mayn/qingju/assignment/AssignmentFragment.java
@@ -7,15 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 import com.example.mayn.qingju.R;
 import com.example.mayn.qingju.customcontrol.ObservableScrollView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 /**
@@ -24,14 +25,22 @@ import butterknife.InjectView;
  */
 
 public class AssignmentFragment extends Fragment {
-    @InjectView(R.id.tv_topView)
-    TextView tvTopView;
+    //@InjectView(R.id.tv_topView)
+    //TextView tvTopView;
     @InjectView(R.id.ll_topView)
     LinearLayout llTopView;
     @InjectView(R.id.sv_contentView)
     ObservableScrollView svContentView;
     @InjectView(R.id.ll_fixedView)
     LinearLayout llFixedView;
+    @InjectView(R.id.button)
+    Button button;
+    @InjectView(R.id.button2)
+    Button button2;
+    @InjectView(R.id.button3)
+    Button button3;
+    @InjectView(R.id.rela_assitant)
+    RelativeLayout relaAssitant;
     private int mHeight;
     //private TextView mTextView;
 
@@ -78,14 +87,14 @@ public class AssignmentFragment extends Fragment {
             @Override
             public void onObservableScrollViewScrollChanged(int l, int t, int oldl, int oldt) {
                 if (t >= llTopView.getTop()) {
-                    if (tvTopView.getParent() != llFixedView) {
-                        llTopView.removeView(tvTopView);
-                        llFixedView.addView(tvTopView);
+                    if (relaAssitant.getParent() != llFixedView) {
+                        llTopView.removeView(relaAssitant);
+                        llFixedView.addView(relaAssitant);
                     }
                 } else {
-                    if (tvTopView.getParent() != llTopView) {
-                        llFixedView.removeView(tvTopView);
-                        llTopView.addView(tvTopView);
+                    if (relaAssitant.getParent() != llTopView) {
+                        llFixedView.removeView(relaAssitant);
+                        llTopView.addView(relaAssitant);
                     }
                 }
 
@@ -107,4 +116,15 @@ public class AssignmentFragment extends Fragment {
         ButterKnife.reset(this);
     }
 
+    @OnClick({R.id.button, R.id.button2, R.id.button3})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button://综合排序
+                break;
+            case R.id.button2://价格
+                break;
+            case R.id.button3://地区
+                break;
+        }
+    }
 }
